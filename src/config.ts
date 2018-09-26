716e0169c88842fc9e1e52d5b3707e4ce3c10efd
ssribeiro <luan@ssribeiro.com>
2018-09-26 11:17:25 -0300

    installed angstone/node-util and environment configuration tool

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+
+const default_file_path = `${__dirname}/../.env`;
+
+export const config = (config?: {file_path?:string}) => {
+  if(!config) config = {};
+  dotenv.config({ path: config.file_path || default_file_path });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("typescript traspiling")
+import * as ast from '@angstone/node-util';
+import { config } from './config';
+
+config();
+
+ast.info('configured environment: '+process.env.NODE_ENV);
