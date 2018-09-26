1863ab56b75df8418edb85158af40be777c9e06b
ssribeiro <luan@ssribeiro.com>
2018-09-26 12:02:20 -0300

    prepared to receiv express portal app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import * as ast from '@angstone/node-util';
 import { config } from './config';
 import { error } from './error';
+import { App } from './app';
 
+/* Boot Process */
 ast.log('booting monostone framework')
 ast.log('loading configuration')
+
+// Config
 config();
 if(process.env.NODE_ENV=='production'||process.env.NODE_ENV=='development')
   ast.success('configuration loaded');
-else error.fatal('configuration failed');
-
-
-
-
-
+else error.fatal('configuration failed - please verify .env file');
 ast.info('configured environment: '+process.env.NODE_ENV);
+
+// Create Express App
+ast.log('creating express app');
+const app:App = App();
+if(app) ast.success('express app created');
+else error.fatal('fail in creating express app');
+
+// Start Express App
+ast.log('starting express App');
+app.start();
+ast.success('express app successfully started');
