86aa4c81380b05767148b2a3bcca122a7803af19
ssribeiro <luan@ssribeiro.com>
2018-09-26 12:48:53 -0300

    express app bed created

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import * as ast from '@angstone/node-util';
+
+export class App {
+
+  constructor() {
+  }
+
+  start() {
+  }
+
+}
+
+export const createApp = function() {
+  return new App();
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as ast from '@angstone/node-util';
 import { config } from './config';
 import { error } from './error';
-import { App } from './app';
+import { App, createApp } from './app';
 
 /* Boot Process */
 ast.log('booting monostone framework')
@@ -16,11 +16,11 @@ ast.info('configured environment: '+process.env.NODE_ENV);
 
 // Create Express App
 ast.log('creating express app');
-const app:App = App();
+const app:App = createApp();
 if(app) ast.success('express app created');
 else error.fatal('fail in creating express app');
 
 // Start Express App
-ast.log('starting express App');
+ast.log('starting express app');
 app.start();
 ast.success('express app successfully started');
